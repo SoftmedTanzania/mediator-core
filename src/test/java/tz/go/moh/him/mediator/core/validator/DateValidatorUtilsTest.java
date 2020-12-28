0a3190bb0ffaf8538e6d6a9ab5a39d71f70b0601
cozej4 <ilakozejumanne@gmail.com>
2020-12-28 13:43:18 +0300

    Code refactoring

--- src/test/java/tz/go/moh/him/mediator/core/validator/DateValidatorUtilsTest.java
+++ src/test/java/tz/go/moh/him/mediator/core/validator/DateValidatorUtilsTest.java
@@ -5,30 +5,30 @@ import org.junit.Test;
 
 import java.util.Calendar;
 
-public class DateValidatorTest {
+public class DateValidatorUtilsTest {
     @Test
     public void testDateAfterGivenStartDateRange() {
         //Checking if the date is after a month ago
-        Assert.assertTrue(DateValidator.isThisDateAfterFieldRange("01/01/2022", "dd/MM/yyyy", Calendar.MONTH, -1));
+        Assert.assertTrue(DateValidatorUtils.isThisDateAfterFieldRange("01/01/2022", "dd/MM/yyyy", Calendar.MONTH, -1));
     }
 
     @Test
     public void testDateAfterBeforeStartDateRange() {
         //Checking if the date is before a month from now
-        Assert.assertTrue(DateValidator.isThisDateBeforeFieldRange("31/01/2012", "dd/MM/yyyy", Calendar.MONTH, 1));
+        Assert.assertTrue(DateValidatorUtils.isThisDateBeforeFieldRange("31/01/2012", "dd/MM/yyyy", Calendar.MONTH, 1));
 
     }
 
     @Test
     public void testIsValidFutureDate() {
         //Checking if the date is a valid future date
-        Assert.assertTrue(DateValidator.isValidFutureDate("01/01/2030", "dd/MM/yyyy"));
+        Assert.assertTrue(DateValidatorUtils.isValidFutureDate("01/01/2030", "dd/MM/yyyy"));
 
     }
 
     @Test
     public void testIsValidPastDate() {
         //Checking if the date is a valid past date
-        Assert.assertTrue(DateValidator.isValidPastDate("01/01/2020", "dd/MM/yyyy"));
+        Assert.assertTrue(DateValidatorUtils.isValidPastDate("01/01/2020", "dd/MM/yyyy"));
     }
 }

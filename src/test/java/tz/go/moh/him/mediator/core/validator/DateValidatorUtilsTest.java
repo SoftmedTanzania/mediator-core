c060014e6756be928e131e28428f3d10362d9f82
cozej4 <ilakozejumanne@gmail.com>
2020-12-29 11:31:50 +0300

    Added additional tests

--- src/test/java/tz/go/moh/him/mediator/core/validator/DateValidatorUtilsTest.java
+++ src/test/java/tz/go/moh/him/mediator/core/validator/DateValidatorUtilsTest.java
@@ -10,12 +10,18 @@ public class DateValidatorUtilsTest {
     public void testDateAfterGivenStartDateRange() {
         //Checking if the date is after a month ago
         Assert.assertTrue(DateValidatorUtils.isThisDateAfterFieldRange("01/01/2022", "dd/MM/yyyy", Calendar.MONTH, -1));
+
+        //Test using wrong date format should always return false
+        Assert.assertFalse(DateValidatorUtils.isThisDateAfterFieldRange("01/01/2022", "ddMMyyyy", Calendar.MONTH, -1));
     }
 
     @Test
     public void testDateAfterBeforeStartDateRange() {
         //Checking if the date is before a month from now
         Assert.assertTrue(DateValidatorUtils.isThisDateBeforeFieldRange("31/01/2012", "dd/MM/yyyy", Calendar.MONTH, 1));
+
+        //Test using wrong date format should always return false
+        Assert.assertFalse(DateValidatorUtils.isThisDateBeforeFieldRange("31/01/2012", "ddMMyyyy", Calendar.MONTH, 1));
 
     }
 
